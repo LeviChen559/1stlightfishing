bf127f1155643bd4155fd65f6e4c26eff0d7ebd1
LeviChen559 <j89311025@gmail.com>
2022-09-25 22:37:29 -0700

    fixed the Logo issue

--- component/logo/index.tsx
+++ component/logo/index.tsx
@@ -2,15 +2,15 @@ import React,{FC,memo} from 'react'
 import Image from 'next/image'
 import { ImageBox,Props } from './style'
 
-const Logo:FC = memo<Props>(({height,width})=> {
+const Logo:FC<Props> =({height,width})=> {
   console.log("Logo")
   return (
     <ImageBox height={height} width={width}>
     <Image src="/logo.svg" objectFit="contain" alt="logo" layout='fill'  />
 </ImageBox>
   );
-});
+};
 
-Logo.displayName = 'Logo';
+// Logo.displayName = 'Logo';
 
 export default Logo
